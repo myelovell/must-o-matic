0dfa22dd18d0e657ce3dc20aaecf3d22963d1bbc
melwin-bruun-nti-johanneberg <melwin.bruun@elev.ga.ntig.se>
2021-12-03 14:16:29 +0100

    new stuff

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :must
+  belongs_to :user
   validates :rating, presence: true, 
                      numericality: {greater_than_or_equal_to: 1, 
                                     less_than_or_equal_to: 10}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_123524) do
+ActiveRecord::Schema.define(version: 2021_12_03_120556) do
 
   create_table "musts", force: :cascade do |t|
     t.string "name"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2021_12_02_123524) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["must_id"], name: "index_ratings_on_must_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
